ca3f312b6bdfaf07b87614d7d501208e9ebb7996
David Li <li.davidm96@gmail.com>
2018-02-23 14:43:34 -0500

    Add basic inventory layout

--- src/components/Inventory.tsx
+++ src/components/Inventory.tsx
@@ -1,6 +1,32 @@
 import * as immutable from "immutable";
 import * as React from "react";
+
+import * as model from "../model/model";
+
 import "../Common.css";
+import LabeledPanel from "./LabeledPanel";
+import Meter from "./Meter";
+
+interface InventoryProps {
+    resources: immutable.Map<model.Resource, number>;
+}
 
 export default class Inventory extends React.Component {
+    render() {
+        return (
+            <div id="inventory" title="Inventory">
+                <LabeledPanel title="Money" id="money-meter">
+                    <div>
+                        Yen
+                    </div>
+                    <div>
+                        0
+                    </div>
+                </LabeledPanel>
+                <LabeledPanel title="Stamina">
+                    Stamina: 99/99
+                </LabeledPanel>
+            </div>
+        );
+    }
 }
