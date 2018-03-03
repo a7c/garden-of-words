74ef89b9fe3ff6fd06d05242cc1d56a18c182996
a7c <ayc48@cornell.edu>
2018-03-03 12:00:39 -0500

    fixed condition for locking wander button

--- src/components/ActionPanel.tsx
+++ src/components/ActionPanel.tsx
@@ -86,7 +86,7 @@ export default class ActionPanel extends React.Component<Props> {
                         cost={`${resources.WANDER_STA} STA`}
                         onClick={this.wander}
                         paused={paused}
-                        locked={stamina <= 0}
+                        locked={stamina <= resources.WANDER_STA}
                         cooldown={1000}
                     />
                     {learned.size ?
