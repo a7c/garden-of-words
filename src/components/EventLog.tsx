a1e3b56eee2e98d500f415cb0201da000c80af5c
David Li <li.davidm96@gmail.com>
2018-03-02 20:50:31 -0500

    Scroll event log to top when new message comes in
    Fixes #32.

--- src/components/EventLog.tsx
+++ src/components/EventLog.tsx
@@ -13,11 +13,21 @@ interface Props {
 }
 
 export default class EventLog extends React.Component<Props> {
+    // Dummy element, used to scroll event log to bottom
+    dummy: HTMLElement | null;
+
+    componentDidUpdate() {
+        if (this.dummy) {
+            this.dummy.scrollIntoView({ behavior: "smooth" });
+        }
+    }
+
     render() {
         const { entries } = this.props;
         const latest = entries.slice().reverse();
         return (
             <LabeledPanel title="Event Log" id="event-log">
+                <div ref={el => (this.dummy = el)}/>
                 <ul>
                     <TransitionGroup>
                         {latest.map((entry, id) => (
