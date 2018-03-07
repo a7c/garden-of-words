4336d0f93bc084d40d534fa724abe1cc19590b80
David Li <li.davidm96@gmail.com>
2018-03-06 20:56:23 -0500

    Embed map as SVG so it's interactable

--- src/components/Map.tsx
+++ src/components/Map.tsx
@@ -1,12 +1,14 @@
 import * as immutable from "immutable";
 import * as React from "react";
+
 import "../Common.css";
+import "./Map.css";
 
 export default class Map extends React.Component {
     render() {
         return (
             <section id="map">
-                <img src={require("../assets/map.svg")} />
+                <object data={require("../assets/map.svg")} type="image/svg+xml" />
             </section>
         );
     }
