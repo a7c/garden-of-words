71d3a5acf1f0eb95767724bd044c905330fa289b
David Li <li.davidm96@gmail.com>
2018-03-13 21:39:01 -0400

    Define richer location data

--- src/data/locations.ts
+++ src/data/locations.ts
@@ -1,7 +1,19 @@
+import * as model from "../model/model";
+
 const locations = require("./locations.json");
+
+export interface PointOfInterest {
+    label: string;
+    eventSource: model.Location;
+    flag: model.Flag;
+}
 
 export interface LocationData {
     name: string;
+    pois: PointOfInterest[];
+    connected: model.Location[];
+
+    wanderlust: boolean;
 }
 
 export default locations as { [key: string]: LocationData };
