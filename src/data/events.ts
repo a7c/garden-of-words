caef595d7c6ef27aa5e13aeafdf6037194a34cd1
David Li <li.davidm96@gmail.com>
2018-03-29 18:26:03 -0400

    Add blue and green ramen as goals

--- src/data/events.ts
+++ src/data/events.ts
@@ -10,6 +10,8 @@ export default {
     airportGate: events,
     airportFoodCourt: require("./collections/food-court.json").map(parsers.parseEvent),
     airportFoodCourtRamenYa: require("./collections/food-court-ramen-ya.json").map(parsers.parseEvent),
+    airportFoodCourtRamenYaBlue: require("./collections/food-court-ramen-ya-blue.json").map(parsers.parseEvent),
+    airportFoodCourtRamenYaGreen: require("./collections/food-court-ramen-ya-green.json").map(parsers.parseEvent),
     transliterateJob: transliterateEventJson.map(parsers.parseEvent),
     vendingMachine: vendingEventJson.map(parsers.parseEvent),
     luggageJob: luggageEventJson.map(parsers.parseEvent)
