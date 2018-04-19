bc3249885e542e1649e7c748325a359bc8acc9f7
a7c <ayc48@cornell.edu>
2018-04-19 06:24:37 -0400

    screw tabs

--- src/model/resources.ts
+++ src/model/resources.ts
@@ -7,12 +7,12 @@ export const LUGGAGE_STA_COST = 25;
 export const LUGGAGE_UNLOCK_STAMINA = 11;
 
 export function getMeditateStaminaCost (store: model.Store): number {
-	const maxStamina = store.get("resources").get(STAMINA).maxValue;
-	if (maxStamina !== null) {
-		return maxStamina / 2;
-	}
-	// shouldn't happen -- there should be a cap on max stamina
-	else {
-		return 10;
-	}
+    const maxStamina = store.get("resources").get(STAMINA).maxValue;
+    if (maxStamina !== null) {
+        return maxStamina / 2;
+    }
+    // shouldn't happen -- there should be a cap on max stamina
+    else {
+        return 10;
+    }
 }
