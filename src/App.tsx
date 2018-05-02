0c168e3e08e6b1830240bcb17c5cd8039abf4558
a7c <ayc48@cornell.edu>
2018-05-02 13:07:01 -0400

    japen grammer

--- src/App.tsx
+++ src/App.tsx
@@ -356,7 +356,7 @@ export default class App extends React.Component {
                 <h1>I Sudoku'd And Was Reborn in Japan, But I'm a
                 Ninja Stuck in Narita Airport And I Don't Speak Any
                 Japanese</h1>
-                <h1>自殺したら、成田空港から脱出できない忍者として転生されたが、日本語が全く喋れない</h1>
+                <h1>自殺したら、成田空港から脱出できない忍者として転生したが、日本語が全く喋れない‼︎</h1>
                 <Test/>
                 <a href="credits.html">Credits</a>
             </div>
