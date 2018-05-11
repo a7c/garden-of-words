ea87d739983eb15e8bce2a29cf03d181d6c08a2e
Michael Mauer <mrm355@cornell.edu>
2018-05-10 22:54:29 -0400

    make memes more mainstream

--- src/components/ActionPanel.tsx
+++ src/components/ActionPanel.tsx
@@ -51,7 +51,7 @@ export default class ActionPanel extends React.Component<Props> {
 
     onHint = (hint: string) => {
         const hintText = Math.random() > 0.8 ?
-                         "Prof. Andersen: there's a time and a place for everything, but not now!" :
+                         "Your Professor's words echoed - there's a time and a place for everything, but not now!" :
                          hint;
         this.props.onEvent(new event.FlavorEvent([], [], hintText));
     }
