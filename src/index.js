c5c5a0f950b8781f15afdb7b578ac421146e40a4
David Li <li.davidm96@gmail.com>
2018-05-11 10:18:25 -0400

    Service worker, go die in a fire

--- src/index.js
+++ src/index.js
@@ -2,6 +2,11 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-import registerServiceWorker from './registerServiceWorker';
 
 ReactDOM.render(<App />, document.getElementById('root'));
+
+navigator.serviceWorker.getRegistrations().then((registrations) => {
+    for (const registration of registrations) {
+        registration.unregister();
+    }
+});
